d9db79c72cb528167aeb39687923e83af526c8bb
Fangzhou Chen <fangzhou@fydeos.io>
2021-08-10 23:30:35 +0800

    add fydeos account switch
    Signed-off-by: Fangzhou Chen <fangzhou@fydeos.io>

---
+++ fydeos/switches/account/account_switches.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 The FydeOS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_FYDEOS_SWITCHES_ACCOUNT_SWITCHES_H_
+#define CHROMEOS_FYDEOS_SWITCHES_ACCOUNT_SWITCHES_H_
+
+#include <string>
+#include "chromeos/chromeos_export.h"
+
+namespace fydeos {
+namespace switches {
+
+CHROMEOS_EXPORT bool IsFydeAccountEnabled();
+
+}
+}
+
+#endif
