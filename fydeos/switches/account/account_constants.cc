d9e79a99a0d306d36a8bc9b74616f40dd6c70d56
Fangzhou Chen <fangzhou@fydeos.io>
2021-08-11 12:04:52 +0800

    add switches for online account and related services
    Signed-off-by: Fangzhou Chen <fangzhou@fydeos.io>

---
+++ fydeos/switches/account/account_constants.cc
@@ -0,0 +1,23 @@
+// Copyright (c) 2019 The FydeOS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fydeos/switches/account/account_constants.h"
+
+namespace fydeos {
+namespace constants {
+
+const char kDefaultFydeOSGaiaUrl[] = "https://account.fydeos.com";
+const char kDefaultFydeOSApisBaseUrl[] = "https://apis.fydeos.com";
+const char kDefaultFydeOSDeviceManagementServerUrl[] = "https://m.fydeos.com/devicemanagement/data/api";
+const char kDefaultFydeOSRealtimeReportingServerUrl[] = "https://apis.fydeos.com/report/events";
+const char kDefaultFydeOSEncryptedReportingServerUrl[] = "https://apis.fydeos.com/report/record";
+const char kFydeOSSyncDevServerUrl[] = "https://clients4.fydeos.com/chrome-sync/dev";
+const char kFydeOSSyncServerUrl[] = "https://clients4.fydeos.com/chrome-sync";
+
+const char kDefaultFydeOSFamilyLinkApisUrl[] = "https://familylink-apis.fydeos.com/kidsmanagement/v1/";
+const char kDefaultFydeOSClassifyURLRequestApiPath[] = "https://familylink-apis.fydeos.com/kidsmanagement/v1/people/me:classifyUrl";
+const size_t kFydeOSSupervisedUserSettingsDefaultSyncIntervalInSeconds = 600;
+
+} //constants
+} //fydeos
