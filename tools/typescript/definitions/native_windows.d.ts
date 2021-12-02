b8d24a0a52c5203a8a4184ce6b8bc93bacdb440e
Fangzhou Chen <fangzhou@fydeos.io>
2021-12-02 11:12:41 +0800

    add fydeos private api typescript declaration
    * app_management
    * native_windows
    
    used by settings

---
+++ tools/typescript/definitions/native_windows.d.ts
@@ -0,0 +1,42 @@
+// Copyright 2021 Fyde Innovations. All rights reserved.
+
+/** @fileoverview Definitions for chrome.nativeWindows API */
+
+declare namespace chrome {
+  export namespace nativeWindows {
+    export enum WindowType {
+      normal = 'normal',
+      popup = 'popup',
+      panel = 'panel',
+      app = 'app',
+      devtools = 'devtools',
+    }
+
+    export enum WindowState {
+      normal = 'normal',
+      minimized = 'minimized',
+      maximized = 'maximized',
+      fullscreen = 'fullscreen',
+      docked = 'docked',
+      locked_fullscreen = 'locked-fullscreen',
+    }
+
+    export interface Window {
+      id?: number;
+      focused: boolean;
+      top?: number;
+      left?: number;
+      width?: number;
+      height?: number;
+      incognito: boolean;
+      type?: WindowType;
+      state?: WindowState;
+      alwaysOnTop: boolean;
+      sessionId?: string;
+      appId?: string;
+      launchId?: string
+    }
+
+    export function create(appId: string, callback?: (window?: Window) => void): void;
+  }
+}
