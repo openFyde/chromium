998211e533da0dc73fd5e98ca566e035ff8e3f55
Fangzhou Chen <fangzhou@fydeos.io>
2021-12-10 15:56:10 +0800

    avoid force_online_signin for fyde local account

--- chrome/browser/ash/login/user_online_signin_notifier.cc
+++ chrome/browser/ash/login/user_online_signin_notifier.cc
@@ -35,6 +35,7 @@ void UserOnlineSigninNotifier::CheckForPolicyEnforcedOnlineSignin() {
       min_delta = time_to_next_online_signin;
     }
     if (time_to_next_online_signin <= base::TimeDelta() &&
+        !user->IsFlintAccountUser() &&
         !user->force_online_signin()) {
       user_manager::UserManager::Get()->SaveForceOnlineSignin(
           user->GetAccountId(), true);
