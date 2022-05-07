0a415a19bb99aee10234e7925663ba6787261930
Chen Fangzhou <fangzhou@fydeos.io>
2022-05-07 02:44:14 +0000

    add fydeos network detector url constant

---
+++ fydeos/switches/urls/urls_constants.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 The FydeOS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_FYDEOS_SWITCHES_URLS_CONSTANTS_
+#define CHROMEOS_FYDEOS_SWITCHES_URLS_CONSTANTS_
+
+#include <string>
+#include "chromeos/chromeos_export.h"
+
+namespace fydeos {
+namespace constants {
+
+CHROMEOS_EXPORT extern const char kDefaultTestUrl[];
+
+} // constants
+} // fydeos
+
+#endif
