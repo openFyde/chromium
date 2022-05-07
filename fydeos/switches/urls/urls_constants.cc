d1ce52e5713ab1c7877d36d6f37834da6fcddc37
Chen Fangzhou <fangzhou@fydeos.io>
2022-05-07 02:44:14 +0000

    add fydeos network detector url constant

---
+++ fydeos/switches/urls/urls_constants.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 The FydeOS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fydeos/switches/urls/urls_constants.h"
+
+
+namespace fydeos {
+namespace constants {
+
+const char kDefaultTestUrl[] = "http://store.fydeos.com/204";
+
+} // constants
+} // fydeos
