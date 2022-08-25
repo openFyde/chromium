e0fa79909a173d6e900bd46f2f5594809f4b876c
Chen Fangzhou <fangzhou@fydeos.io>
2022-08-25 14:57:05 +0800

    switches for fydeos geolocation and timezone api

---
+++ fydeos/switches/services/services_constants.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 The FydeOS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fydeos/switches/services/services_constants.h"
+
+namespace fydeos {
+namespace constants {
+
+const char kDefaultFydeOSGeolocationAPIUrl[] = "https://apis.fydeos.com/geo/locationByIp?";
+const char kDefaultFydeOSTimezoneAPIUrl[] = "https://apis.fydeos.com/geo/timezone?";
+
+} // constants
+} // fydeos
