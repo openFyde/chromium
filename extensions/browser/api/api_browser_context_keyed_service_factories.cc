ac878147c35ad44dbf1b549e932a381895c2534a
Chen Fangzhou <fangzhou@fydeos.io>
2025-02-12 13:16:07 +0000

    fix compile error of patches.base/0004-add-fydeos-shellclient-api.patch

--- extensions/browser/api/api_browser_context_keyed_service_factories.cc
+++ extensions/browser/api/api_browser_context_keyed_service_factories.cc
@@ -66,6 +66,8 @@
 #include "extensions/browser/api/virtual_keyboard_private/virtual_keyboard_private_api.h"  // nogncheck
 #endif
 
+#include "fydeos/extensions/browser/api/shell_client/shell_client_api.h"
+
 namespace extensions {
 
 void EnsureApiBrowserContextKeyedServiceFactoriesBuilt() {
@@ -77,6 +79,7 @@ void EnsureApiBrowserContextKeyedServiceFactoriesBuilt() {
   StorageFrontend::GetFactoryInstance();
   WebRequestAPI::GetFactoryInstance();
   WebRequestProxyingURLLoaderFactory::EnsureAssociatedFactoryBuilt();
+  ShellClientAPI::GetFactoryInstance();
 
 // The following are not supported in the experimental desktop-android build.
 // TODO(https://crbug.com/356905053): Enable these APIs on desktop-android.
