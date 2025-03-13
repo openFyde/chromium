1aa2ad59bd4034d76114a005499ea16f6cb11de5
Chen Fangzhou <fangzhou@fydeos.io>
2025-03-13 02:19:38 +0000

    patches/patches.base/0404-disable-google-icon-of-home-button.patch

--- ash/shelf/home_button.cc
+++ ash/shelf/home_button.cc
@@ -65,6 +65,7 @@
 #include "ui/views/style/typography.h"
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
+#include "build/branding_buildflags.h"
 
 namespace ash {
 namespace {
@@ -277,10 +278,12 @@ class HomeButton::ButtonImageView : public views::View {
         image_model_ =
             ui::ImageModel::FromVectorIcon(kCampbell9dotIcon, GetIconColorId());
       }
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
     } else if (Shell::Get()->keyboard_capability()->GetMetaKeyToDisplay() ==
                ui::mojom::MetaKey::kLauncherRefresh) {
       image_model_ =
           ui::ImageModel::FromVectorIcon(kCampbellHeroIcon, GetIconColorId());
+#endif
     } else {
       image_model_ = std::nullopt;
       image_ = gfx::ImageSkia();
