67f9080c92b16b816b41f1f19dc4978cec6d58fc
Chen Fangzhou <fangzhou@fydeos.io>
2025-04-14 09:45:24 +0000

    clean local auto signin after resetting password

--- chrome/browser/ui/webui/settings/people_handler.cc
+++ chrome/browser/ui/webui/settings/people_handler.cc
@@ -76,6 +76,9 @@
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
+#include "components/prefs/pref_service.h"
+#include "chrome/browser/browser_process.h"
+#include "fydeos/prefs/fydeos_pref_names.h"
 #endif
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
@@ -345,6 +348,10 @@ void PeopleHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
       "TurnOffSync", base::BindRepeating(&PeopleHandler::HandleTurnOffSync,
                                          base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+    "cleanLocalAutoSignin",
+    base::BindRepeating(&PeopleHandler::HandleCleanLocalAutoSignin,
+                        base::Unretained(this)));
 #else
   web_ui()->RegisterMessageCallback(
       "SyncSetupStartSignIn",
@@ -717,6 +724,17 @@ void PeopleHandler::HandleTurnOnSync(const base::Value::List& args) {
 void PeopleHandler::HandleTurnOffSync(const base::Value::List& args) {
   NOTREACHED() << "It is not possible to toggle Sync on Ash";
 }
+
+void PeopleHandler::HandleCleanLocalAutoSignin(const base::Value::List& args) {
+  if (!g_browser_process) {
+    return;
+  }
+  PrefService* prefs = g_browser_process->local_state();
+  prefs->SetString(fydeos::prefs::kOfflineAutoSigninPassword, std::string());
+  prefs->SetString(fydeos::prefs::kOfflineAutoSigninAccountIdKey,
+      std::string());
+}
+
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
