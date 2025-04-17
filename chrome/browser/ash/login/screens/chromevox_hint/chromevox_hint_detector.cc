88c026c548be72a375fbdb24ba9a61fb3da38d25
Chen Fangzhou <fangzhou@fydeos.io>
2025-04-17 05:23:13 +0000

    temp

--- chrome/browser/ash/login/screens/chromevox_hint/chromevox_hint_detector.cc
+++ chrome/browser/ash/login/screens/chromevox_hint/chromevox_hint_detector.cc
@@ -10,12 +10,13 @@
 #include "base/functional/bind.h"
 #include "chromeos/ash/experiences/idle_detector/idle_detector.h"
 #include "chromeos/dbus/constants/dbus_switches.h"
+#include "fydeos/switches/misc/misc_switches.h"
 
 namespace ash {
 namespace {
 
 // Amount of time the user has to be idle for before giving the ChromeVox hint.
-const base::TimeDelta kChromeVoxHintIdleDuration = base::Seconds(20);
+const base::TimeDelta kChromeVoxHintIdleDuration = base::Seconds(180);
 
 }  // namespace
 
@@ -32,6 +33,9 @@ void ChromeVoxHintDetector::StartIdleDetection() {
   if (switches::IsOOBEChromeVoxHintTimerDisabledForTesting()) {
     return;
   }
+  // if (fydeos::switches::IsFydeCustomEnabled()) {
+  //   return;
+  // }
 
   // This is done so that developers and testers don't repeatedly receive
   // the hint when flashing.
